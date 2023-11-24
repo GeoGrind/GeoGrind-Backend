d5cc2c08a5c632ba0623ae59afc84f5ae65af96e
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2023-11-24 17:36:19 -0500

    docker working in progress

---
+++ startServer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+  source .env
+fi
+
+# Check if the container with the same name already exists
+if [ "$(docker ps -aq -f name=geogrind-backend)" ]; then
+  # If the container exists, stop and remove it
+  docker stop geogrind-backend
+  docker rm geogrind-backend
+fi
+
+# main server
+echo "Connecting to GeoGrind Server at $SERVER_HOST:$SERVER_PORT"
+
+# database server
+echo "Connecting to GeoGrind Database at $DB_HOST:$DB_PORT"
+
+# redis server
+echo "Connecting to GeoGrind Redis Server at $REDIS_HOST:$REDIS_PORT"
+
+# Start the Docker container
+docker-compose up -d
+
+echo "GeoGrind Server Container started."
+echo "GeoGrind Database Container started."
+echo "GeoGrind Redis Container started."
+
+# Check the logs to see if everything started successfully
+docker-compose logs
